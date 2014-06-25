a748adb488be1fa3033940e2ef59016fcd161301
pauldardeau <pauldardeau@me.com>
2014-06-25 10:39:45 -0500

    Changed members to be private, added copy/move constructors and operators, adding lifecycle logging

--- HttpServer/Utils/SystemInfo.h
+++ HttpServer/Utils/SystemInfo.h
@@ -13,14 +13,30 @@
 class SystemInfo
 {
 public:
-   SystemInfo();
-   ~SystemInfo();
+   SystemInfo() noexcept;
+   SystemInfo(const SystemInfo& copy) noexcept;
+   SystemInfo(SystemInfo&& move) noexcept;
+   ~SystemInfo() noexcept;
    
+   SystemInfo& operator=(const SystemInfo& copy) noexcept;
+   SystemInfo& operator=(SystemInfo&& move) noexcept;
+   
+   const std::string& sysName() const noexcept;
+   const std::string& nodeName() const noexcept;
+   const std::string& release() const noexcept;
+   const std::string& version() const noexcept;
+   const std::string& machine() const noexcept;
+   
+   bool retrievedSystemInfo() const noexcept;
+
+   
+private:
    std::string m_sysName;
    std::string m_nodeName;
    std::string m_release;
    std::string m_version;
    std::string m_machine;
+   bool m_retrievedSystemInfo;
    
 };
 
