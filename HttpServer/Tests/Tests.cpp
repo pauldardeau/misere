bee322eddefccff0eee3db17a8241e353a9dbe66
pauldardeau <pauldardeau@me.com>
2014-07-27 16:12:02 -0500

    removed tests that are now in chaudiere

--- HttpServer/Tests/Tests.cpp
+++ HttpServer/Tests/Tests.cpp
@@ -10,22 +10,6 @@
 #include "TestHttpResponse.h"
 #include "TestHttpServer.h"
 #include "TestHttpTransaction.h"
-#include "TestIniReader.h"
-#include "TestInvalidKeyException.h"
-#include "TestKeyValuePairs.h"
-#include "TestKqueueServer.h"
-#include "TestPthreadsMutex.h"
-#include "TestStdMutex.h"
-#include "TestRequestHandler.h"
-#include "TestServerSocket.h"
-#include "TestSocket.h"
-#include "TestSocketRequest.h"
-#include "TestStringTokenizer.h"
-#include "TestStrUtils.h"
-#include "TestThread.h"
-#include "TestThreadPool.h"
-#include "TestThreadPoolQueue.h"
-#include "TestThreadPoolWorker.h"
 
 
 void Tests::run()
@@ -50,52 +34,4 @@ void Tests::run()
    
    TestHttpTransaction testHttpTransaction;
    testHttpTransaction.run();
-   
-   TestIniReader testIniReader;
-   testIniReader.run();
-   
-   TestInvalidKeyException testIKE;
-   testIKE.run();
-   
-   TestKeyValuePairs testKVP;
-   testKVP.run();
-   
-   TestKqueueServer testKqueueServer;
-   testKqueueServer.run();
-   
-   TestPthreadsMutex testPthreadsMutex;
-   testPthreadsMutex.run();
-
-   TestStdMutex testStdMutex;
-   testStdMutex.run();
-
-   TestRequestHandler testRequestHandler;
-   testRequestHandler.run();
-   
-   TestServerSocket testServerSocket;
-   testServerSocket.run();
-   
-   TestSocket testSocket;
-   testSocket.run();
-   
-   TestSocketRequest testSocketRequest;
-   testSocketRequest.run();
-   
-   TestStringTokenizer testStringTokenizer;
-   testStringTokenizer.run();
-   
-   TestStrUtils testStrUtils;
-   testStrUtils.run();
-   
-   TestThread testThread;
-   testThread.run();
-   
-   TestThreadPool testThreadPool;
-   testThreadPool.run();
-
-   TestThreadPoolQueue testThreadPoolQueue;
-   testThreadPoolQueue.run();
-
-   TestThreadPoolWorker testThreadPoolWorker;
-   testThreadPoolWorker.run();
 }
